d5fc334ace11847e145672e42e58d394217b49ea
Alessandro Agapito <160891152+AlessandroAgapito23@users.noreply.github.com>
2024-07-29 10:05:24 +0200

    Update __init__.py

--- GWFish/modules/__init__.py
+++ GWFish/modules/__init__.py
@@ -1,2 +1,2 @@
 from __future__ import absolute_import
-from . import auxiliary, detection, fishermatrix, waveforms, inspiral_corrections, constants, fft
+from . import auxiliary, detection, fishermatrix, waveforms, constants, fft, inspiral_corrections
